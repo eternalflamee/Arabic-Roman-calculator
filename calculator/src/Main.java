a2cdfbe55eb7681b731fb0287bcfa826b5010ec4
eternalflamee <eternal.flame126@mail.ru>
2022-12-23 02:20:04 +0300

    Fixed: the answer is not given for Roman numbers when the result is greater than 10.

--- calculator/src/Main.java
+++ calculator/src/Main.java
@@ -89,22 +89,13 @@ public class Main {
         };
     }
 
-    public static String numberToRoman(int number) throws CalculatorException {
-        return switch (number) {
-            case 1 -> "I";
-            case 2 -> "II";
-            case 3 -> "III";
-            case 4 -> "IV";
-            case 5 -> "V";
-            case 6 -> "VI";
-            case 7 -> "VII";
-            case 8 -> "VIII";
-            case 9 -> "IX";
-            case 10 -> "X";
-            default -> throw new CalculatorException("Римские цифры не могут быть меньше 1");
-        };
+    public static String numberToRoman (int numArabian) throws CalculatorException {
+        if (numArabian < 0) {
+            throw new CalculatorException("В римской системе счисления нет нулей");
+        }
+        String[] roman = {"string4accuracy", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX", "X", "XI", "XII", "XIII", "XIV", "XV", "XVI", "XVII", "XVIII", "XIX", "XX", "XXI", "XXII", "XXIII", "XXIV", "XXV", "XXVI", "XXVII", "XXVIII", "XXIX", "XXX", "XXXI", "XXXII", "XXXIII", "XXXIV", "XXXV", "XXXVI", "XXXVII", "XXXVIII", "XXXIX", "XL", "XLI", "XLII", "XLIII", "XLIV", "XLV", "XLVI", "XLVII", "XLVIII", "XLIX", "L", "LI", "LII", "LIII", "LIV", "LV", "LVI", "LVII", "LVIII", "LIX", "LX", "LXI", "LXII", "LXIII", "LXIV", "LXV", "LXVI", "LXVII", "LXVIII", "LXIX", "LXX", "LXXI", "LXXII", "LXXIII", "LXXIV", "LXXV", "LXXVI", "LXXVII", "LXXVIII", "LXXIX", "LXXX", "LXXXI", "LXXXII", "LXXXIII", "LXXXIV", "LXXXV", "LXXXVI", "LXXXVII", "LXXXVIII", "LXXXIX", "XC", "XCI", "XCII", "XCIII", "XCIV", "XCV", "XCVI", "XCVII", "XCVIII", "XCIX", "C"};
+        return roman[numArabian];
     }
-
 
     public static int calculate (int number1, int number2, char operation) throws CalculatorException {
         return switch (operation) {
